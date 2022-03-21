74675afcf716f7dd141836c7600c701104b0d929
WildahAlifia <Wildahalif1653@gmail.com>
2022-03-21 13:35:47 +0700

    folder CI

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Pekenalkan</h1>";
+        echo "Nama saya M Yusuf Abdilah
+              Saya tinggal di daerah Gunungsindur
+              olah raga yang saya sukai adalah
+              Bulutangkis";
+    }
+}
