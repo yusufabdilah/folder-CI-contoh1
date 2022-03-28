2695436689fa9c84868fee07e4b46bbc83d8de84
yusufabdilah <yusufabdilah75@gmail.com>
2022-03-28 23:14:21 +0700

    developer

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programing";
+        //$this->load->view('view-latihan1');
+    }
+
+    public function perjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model1_latihan->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan1', $data);
+    }
+}   
---
+++ application/views/view-latihan.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+
+    Halo Kawan.. Yuk Kita Belajar Web Programing..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    
+</body>
+
+</html>
